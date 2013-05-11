2004e67f0fc08ee3fdacb1ae7a88091e2d8aad28
Juan Pablo Civile <elementohb@gmail.com>
2013-05-10 21:09:09 -0300

    DRY on SliderActivity

--- src/com/champo/unsuckyringer/SliderActivity.java
+++ src/com/champo/unsuckyringer/SliderActivity.java
@@ -8,62 +8,40 @@ import android.os.Bundle;
 
 public class SliderActivity extends Activity {
 
-	private AudioManager audioManager;
-	
-	private Slider notificationSlider;
-	private Slider ringerSlider;
-	private Slider smsSlider;
-	
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate(final Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
 		setContentView(R.layout.ringer_widget);
-		audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-		
+		AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
 		int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_RING);
 		int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_RING);
-		
+
 		final SharedPreferences preferences = getSharedPreferences("volumes", 0);
 
-		notificationSlider = (Slider) findViewById(R.id.notification_slider);
-		notificationSlider.setMaxValue(maxVolume);
-		notificationSlider.setSelectedValue(preferences.getInt("notification", currentVolume));
-		notificationSlider.setSliderChangeListener(new Slider.OnSliderChangeListener() {
-			
-			@Override
-			public void onSliderValuesChanged(Slider slider, int value) {
-				preferences.edit()
-					.putInt("notification", value)
-					.commit();
-			}
-		});
-		
-		ringerSlider = (Slider) findViewById(R.id.ringer_slider);
-		ringerSlider.setMaxValue(maxVolume);
-		ringerSlider.setSelectedValue(preferences.getInt("ringer", currentVolume));
-		ringerSlider.setSliderChangeListener(new Slider.OnSliderChangeListener() {
-			
-			@Override
-			public void onSliderValuesChanged(Slider slider, int value) {
-				preferences.edit()
-					.putInt("ringer", value)
-					.commit();
-			}
-		});
-		
-		smsSlider = (Slider) findViewById(R.id.sms_slider);
-		smsSlider.setMaxValue(maxVolume);
-		smsSlider.setSelectedValue(preferences.getInt("sms", currentVolume));
-		smsSlider.setSliderChangeListener(new Slider.OnSliderChangeListener() {
-			
-			@Override
-			public void onSliderValuesChanged(Slider slider, int value) {
-				preferences.edit()
-					.putInt("sms", value)
-					.commit();
-			}
-		});
+		configureSlider(R.id.notification_slider, VolumeType.NOTIFICATION, preferences, currentVolume, maxVolume);
+		configureSlider(R.id.ringer_slider, VolumeType.RINGER, preferences, currentVolume, maxVolume);
+		configureSlider(R.id.sms_slider, VolumeType.SMS, preferences, currentVolume, maxVolume);
 	}
-	
+
+	private void configureSlider(
+			final int viewId, final VolumeType type, final SharedPreferences preferences, final int currentVolume, final int maxVolume) {
+
+		Slider slider = (Slider) findViewById(viewId);
+		slider.setMaxValue(maxVolume);
+		slider.setSliderChangeListener(new Slider.OnSliderChangeListener() {
+
+			@Override
+			public void onSliderValuesChanged(final Slider slider, final int value) {
+				preferences.edit()
+					.putInt(type.name(), value)
+					.commit();
+			}
+		});
+
+		// This has to go after setting the listener so that value in the preferences gets setup
+		slider.setSelectedValue(preferences.getInt(type.name(), currentVolume));
+	}
+
 }
