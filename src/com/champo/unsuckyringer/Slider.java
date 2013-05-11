926b36b07bbc50ab7126da155a4b9052bfa5bdfe
Juan Pablo Civile <elementohb@gmail.com>
2013-05-10 21:08:05 -0300

    Call listener when manually setting the selected value

--- src/com/champo/unsuckyringer/Slider.java
+++ src/com/champo/unsuckyringer/Slider.java
@@ -24,7 +24,7 @@ public class Slider extends ImageView {
 	private final float thumbHalfHeight = 0.5f * thumbImage.getHeight();
 	private final float lineHeight = 0.3f * thumbHalfHeight;
 	private final float padding = thumbHalfWidth;
-	
+
 	private boolean notifyWhileDragging = false;
 	private OnSliderChangeListener listener;
 
@@ -56,25 +56,25 @@ public class Slider extends ImageView {
 
 	private int mScaledTouchSlop;
 	private boolean mIsDragging;
-	
+
 	private int maxValue;
 	private int selectedValue;
-	
-	public Slider(Context context, AttributeSet attrs) {
+
+	public Slider(final Context context, final AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
-	
-	public Slider(Context context, AttributeSet attrs, int defStyle) {
+
+	public Slider(final Context context, final AttributeSet attrs, final int defStyle) {
 	    super(context, attrs, defStyle);
 	    setup(10);
 	}
 
-	public Slider(int maxValue, Context context) throws IllegalArgumentException {
+	public Slider(final int maxValue, final Context context) throws IllegalArgumentException {
 		super(context);
 		setup(maxValue);
 	}
 
-	private void setup(int maxValue) {
+	private void setup(final int maxValue) {
 		this.maxValue = maxValue;
 
 		// make Slider focusable. This solves focus handling issues in
@@ -96,42 +96,45 @@ public class Slider extends ImageView {
 	/**
 	 * Should the widget notify the listener callback while the user is still
 	 * dragging a thumb? Default is false.
-	 * 
+	 *
 	 * @param flag
 	 */
-	public void setNotifyWhileDragging(boolean flag) {
+	public void setNotifyWhileDragging(final boolean flag) {
 		this.notifyWhileDragging = flag;
 	}
 
 	/**
 	 * Registers given listener callback to notify about changed selected
 	 * values.
-	 * 
+	 *
 	 * @param listener
 	 *            The listener to notify about changed selected values.
 	 */
-	public void setSliderChangeListener(OnSliderChangeListener listener) {
+	public void setSliderChangeListener(final OnSliderChangeListener listener) {
 		this.listener = listener;
 	}
-	
-	public void setMaxValue(int maxValue) {
+
+	public void setMaxValue(final int maxValue) {
 		this.maxValue = maxValue;
 	}
-	
+
 	public int getSelectedValue() {
 		return selectedValue;
 	}
-	
-	public void setSelectedValue(int selectedValue) {
+
+	public void setSelectedValue(final int selectedValue) {
 		this.selectedValue = selectedValue;
+		if (listener != null) {
+			listener.onSliderValuesChanged(this, selectedValue);
+		}
 	}
-	
+
 	/**
 	 * Handles thumb selection and movement. Notifies listener callback on
 	 * certain events.
 	 */
 	@Override
-	public boolean onTouchEvent(MotionEvent event) {
+	public boolean onTouchEvent(final MotionEvent event) {
 
 		if (!isEnabled()) {
 			return false;
@@ -156,7 +159,7 @@ public class Slider extends ImageView {
 
 			break;
 		case MotionEvent.ACTION_MOVE:
-			
+
 			if (mIsDragging) {
 				invalidate();
 				trackTouchEvent(event);
@@ -219,7 +222,7 @@ public class Slider extends ImageView {
 		return true;
 	}
 
-	private final void onSecondaryPointerUp(MotionEvent ev) {
+	private final void onSecondaryPointerUp(final MotionEvent ev) {
 		final int pointerIndex = (ev.getAction() & ACTION_POINTER_INDEX_MASK) >> ACTION_POINTER_INDEX_SHIFT;
 
 		final int pointerId = ev.getPointerId(pointerIndex);
@@ -233,14 +236,14 @@ public class Slider extends ImageView {
 		}
 	}
 
-	private final void trackTouchEvent(MotionEvent event) {
+	private final void trackTouchEvent(final MotionEvent event) {
 		final int pointerIndex = event.findPointerIndex(mActivePointerId);
 		final float x = event.getX(pointerIndex);
 
 		setNormalizedValue(screenToNormalized(x));
 	}
 
-	private void setNormalizedValue(double value) {
+	private void setNormalizedValue(final double value) {
 		selectedValue = (int) Math.round(value * maxValue);
 	}
 
@@ -273,8 +276,8 @@ public class Slider extends ImageView {
 	 * Ensures correct size of the widget.
 	 */
 	@Override
-	protected synchronized void onMeasure(int widthMeasureSpec,
-			int heightMeasureSpec) {
+	protected synchronized void onMeasure(final int widthMeasureSpec,
+			final int heightMeasureSpec) {
 		int width = 200;
 		if (MeasureSpec.UNSPECIFIED != MeasureSpec.getMode(widthMeasureSpec)) {
 			width = MeasureSpec.getSize(widthMeasureSpec);
@@ -290,7 +293,7 @@ public class Slider extends ImageView {
 	 * Draws the widget on the given canvas.
 	 */
 	@Override
-	protected synchronized void onDraw(Canvas canvas) {
+	protected synchronized void onDraw(final Canvas canvas) {
 		super.onDraw(canvas);
 
 		// draw seek bar background line
@@ -339,7 +342,7 @@ public class Slider extends ImageView {
 	 * RangeSeekBar widget using the {@link #setId(int)} method.
 	 */
 	@Override
-	protected void onRestoreInstanceState(Parcelable parcel) {
+	protected void onRestoreInstanceState(final Parcelable parcel) {
 		final Bundle bundle = (Bundle) parcel;
 		super.onRestoreInstanceState(bundle.getParcelable("SUPER"));
 		maxValue = bundle.getInt("MAX");
@@ -348,7 +351,7 @@ public class Slider extends ImageView {
 
 	/**
 	 * Draws the "normal" resp. "pressed" thumb image on specified x-coordinate.
-	 * 
+	 *
 	 * @param screenCoord
 	 *            The x-coordinate in screen space where to draw the image.
 	 * @param pressed
@@ -356,31 +359,31 @@ public class Slider extends ImageView {
 	 * @param canvas
 	 *            The canvas to draw upon.
 	 */
-	private void drawThumb(float screenCoord, Canvas canvas) {
+	private void drawThumb(final float screenCoord, final Canvas canvas) {
 		canvas.drawBitmap(thumbImage, screenCoord
 				- thumbHalfWidth,
-				(float) ((0.5f * getHeight()) - thumbHalfHeight), paint);
+				(0.5f * getHeight()) - thumbHalfHeight, paint);
 	}
 
 	/**
 	 * Converts a normalized value into screen space.
-	 * 
+	 *
 	 * @param normalizedCoord
 	 *            The normalized value to convert.
 	 * @return The converted value in screen space.
 	 */
-	private float normalizedToScreen(double normalizedCoord) {
+	private float normalizedToScreen(final double normalizedCoord) {
 		return (float) (padding + normalizedCoord * (getWidth() - 2 * padding));
 	}
 
 	/**
 	 * Converts screen space x-coordinates into normalized values.
-	 * 
+	 *
 	 * @param screenCoord
 	 *            The x-coordinate in screen space to convert.
 	 * @return The normalized value.
 	 */
-	private double screenToNormalized(float screenCoord) {
+	private double screenToNormalized(final float screenCoord) {
 		int width = getWidth();
 		if (width <= 2 * padding) {
 			// prevent division by zero, simply return 0.
@@ -395,7 +398,7 @@ public class Slider extends ImageView {
 	 * Callback listener interface to notify about changed value.
 	 */
 	public interface OnSliderChangeListener {
-		
+
 		public void onSliderValuesChanged(Slider slider, int value);
 	}
 
